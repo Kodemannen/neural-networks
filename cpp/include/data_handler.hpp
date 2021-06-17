4fabf16e57b3a987629b52d9341b2f6c11b0bf03
kodemannen <samuelknudsen@hotmail.com>
2021-06-18 01:30:29 +0200

    ASd

---
+++ cpp/include/data_handler.hpp
@@ -0,0 +1,18 @@
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
+
+#include <fstream>
+#include "stdint.h"
+#include "data.h"
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_set>
+
+class data_handler 
+{
+
+
+}
+
+#endif
