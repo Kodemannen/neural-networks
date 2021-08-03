57b1ac0f543c10ae7bd1beef12188d2087079088
kodemannen <samuelknudsen@hotmail.com>
2021-08-04 01:49:59 +0200

    Forward pass seems to work

--- cpp/include/basic_neural_net.hpp
+++ cpp/include/basic_neural_net.hpp
@@ -1,14 +1,40 @@
-#include <vector>
 #include <armadillo>
+#include <vector>
+#include <map>
+#include <string>
+#include <array>
 
 
+#include <iostream>
+#include <fstream>
+
+#include <stdio.h> // for printf
+
+
+
+// random numbers:
+#include <cstdlib>      // srand() from here?
+#include <ctime>        // and rand() from here?
+
+// for delay:
+#include <unistd.h>
 class neural_net
 {
+    private:
+    arma::mat W;
+    arma::colvec a;
+
     public:
     neural_net(std::vector<int> );
     ~neural_net();
 
+    void forward(arma::colvec input);
 
-    std::vector<int> layers; 
+    int n_layers;
+    std::vector<int> nodes; 
+    std::vector<arma::mat> weight_matrices;
+    std::vector<arma::colvec> biases;
+    std::vector<arma::colvec> pre_activations;
+    std::vector<arma::colvec> activations;
 
 };
--- cpp/src/basic_neural_net.cpp
+++ cpp/src/basic_neural_net.cpp
@@ -1,39 +1,111 @@
 #include "basic_neural_net.hpp"
 
-
-
-
 neural_net::neural_net(std::vector<int> nodes)
 {
-    // nodes contain the number of nodes for all the layers
+    /*
+     * Constructs a neural net object
+     * Contains weights for each layer
+     
+     * Each layer could be an object
+
+    ----------------------------------------------------------------
+    Argument    |   Description
+    ----------------------------------------------------------------
+    nodes       |   vector containing the number of nodes/neurons in each layer
+
+    */
     
-    std::cout << "fitte" << std::endl;
+
+    n_layers = nodes.size();
+
+    //----------------------------------------
+    // Create weight matrices:
+    //----------------------------------------
+    // * Stored in a std::map 
+    arma::arma_rng::set_seed_random(); 
+
+    arma::mat W;
+    arma::colvec b;
+    arma::colvec z;     // prior to activation 
+    arma::colvec a;
+
+
+    // There are always 1 more activation maps than matrices,
+    // so we store the 0th first:
+    a = arma::zeros<arma::colvec>(nodes[0]);  
+    activations.push_back(a);
+
+    for (int i=0; i<n_layers-1; i++)
+    {
+        W = arma::randn<arma::mat>( nodes[i+1], nodes[i] );  // drawn from gaussian(0,1)
+        weight_matrices.push_back(W);
+
+        b = arma::ones<arma::colvec>(nodes[i+1]);
+        biases.push_back(b);
+
+        z = arma::zeros<arma::colvec>(nodes[i+1]);  
+        pre_activations.push_back(z);
+
+        a = arma::zeros<arma::colvec>(nodes[i+1]);  
+        activations.push_back(a);
+    }
+
+
+
 }
+
 
 
 // destructor
 neural_net::~neural_net()
 {
-
 }
 
 
+arma::colvec relu(arma::colvec v)
+{
+    return v%(v>0);
+}
 
+void neural_net::forward(arma::colvec input)
+{
 
+    /* weight_matrices[0].print(); */
+    /* activations[0].print(); */
 
+    // The 0th activation map is the input layer
+    activations[0] = input;
 
+    arma::mat W;
+    arma::colvec b;
+    arma::colvec a_prev;
 
+    for (int i=0; i<n_layers-1; i++)
+    {
+        a_prev = activations[i];
+        W = weight_matrices[i];
+        b = biases[i];
 
+        pre_activations[i] = W*a_prev + b;
+        activations[i+1] = relu(pre_activations[i]);
+        // should apply softmax on the last layer!
+    }
 
-
+}
+    
 
 int main()
 {
     std::cout << "just_testing" << std::endl;
 
     // Define neural net architecture:
-    std::vector<int> layers = {1,2,3};      // first is input layer, last output layer
+    int n_inp=2;
+    std::vector<int> nodes = {n_inp, 3, 4, 2};      // first is input layer, last output layer
+    arma::colvec inp = arma::ones<arma::colvec>(n_inp);
 
-    neural_net nn = neural_net(layers);
+    neural_net nn = neural_net(nodes);
+    nn.forward(inp);
+
+
     return 0;
 }
